1648f1a8c5c8d9f2b7e809674ea23ff20d196420
leamsigc <leamsigc@hotmail.com>
2019-10-21 13:28:26 +0200

    Work whit v-for and methods fromvue

--- 1/ApiCall/app.js
+++ 1/ApiCall/app.js
@@ -1,22 +1,18 @@
 let vm = new Vue({
 	el: '#app',
 	data: {
-		imgUrl: null,
-		loading: true,
-		title: '',
-		explanation: ''
+		asteroids: [],
+		loading: true
 	},
 	methods: {
 		getDataFromApi() {
-			const URL = 'https://api.nasa.gov/planetary/apod?api_key=';
+			const URL = 'https://api.nasa.gov/neo/rest/v1/neo/browse?api_key=';
 			const APIKEY = 'ZHKEOWWz5sedu6TR9yosZwRFClxNWBvXlRzaPdq3';
 			fetch(`${URL}${APIKEY}`)
 				.then(res => res.json())
 				.then(resp => {
-					console.log(resp);
-					this.title = resp.title;
-					this.imgUrl = resp.url;
-					this.explanation = resp.explanation;
+					// console.log(resp.near_earth_objects);
+					this.asteroids = resp.near_earth_objects.slice(0, 10);
 					this.loading = false;
 				});
 		}
